f95a5bb3f87ddb096412b8a8347f6ef8b994f1e8
Mariia Kravchenko <mariia.kravchenko2204@gmail.com>
2024-09-24 19:09:11 -0400

    feat: add logic to get input and display pictures

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,27 +1,48 @@
 const img__container = document.querySelector('.image__container');
+const inputForm = document.querySelector(".form-control");
+const searchButton = document.querySelector(".search__button");
 
-async function getPhotos (param) {
+
+async function getPhotos(param) {
     fetch(`https://api.unsplash.com/search/photos?query=${param}&per_page=18&orientation=landscape&client_id=dbXTMWsnem3eDUnb3j7Kva8_XcWwF7NCZYWt06_6bKk`)
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-        data.results.map(e => {
-            console.log(e.urls.small)
-            const element = createImgElement(e.urls.small);
-            img__container.appendChild(element);
+        .then((res) => res.json())
+        .then((data) => {
+            img__container.innerHTML = '';
+            data.results.map(e => {
+                const element = createImgElement(e.urls.small);
+                img__container.appendChild(element);
+            })
         })
-    })  
 }
 
-function createImgElement (link) {
+function createImgElement(link) {
     const img__element = document.createElement('img');
-  
     img__element.classList.add('img__element');
-   
     img__element.style.background = `url(${link})`;
-    console.log(img__element);
     return img__element;
 }
 
 
-getPhotos('spring')
+getPhotos('Canada')
+
+
+searchButton.addEventListener('click', () => {
+
+    if(inputForm.value === ''){
+        getPhotos('Canada')
+    }else{
+        getPhotos(inputForm.value);
+    }
+    
+})
+
+inputForm.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+        if(inputForm.value === ''){
+            getPhotos('Canada')
+        }else{
+            getPhotos(inputForm.value);
+        }
+    }
+
+})
