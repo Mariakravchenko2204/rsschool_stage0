116e7ef01955c0e10c7608e24d93b8cf2f801f2e
Mariia Kravchenko <mariia.kravchenko2204@gmail.com>
2024-09-25 09:12:00 -0400

    feat: add opening of full size picture by click

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -12,23 +12,26 @@ let maxPages = 5;
 
 
 async function getPhotos() {
-    fetch(`https://api.unsplash.com/search/photos?query=${query}&page=${page}&per_page=18&orientation=landscape&client_id=dbXTMWsnem3eDUnb3j7Kva8_XcWwF7NCZYWt06_6bKk`)
+    fetch(`https://api.unsplash.com/search/photos?query=${query}&page=${page}&per_page=12&orientation=landscape&client_id=dbXTMWsnem3eDUnb3j7Kva8_XcWwF7NCZYWt06_6bKk`)
         .then((res) => res.json())
         .then((data) => {
-            totalPages = data.total_pages;
             img__container.innerHTML = '';
             data.results.map(e => {
-                const element = createImgElement(e.urls.small);
+                const element = createImgElement(e.urls.small, e.urls.full);
                 img__container.appendChild(element);
             })
         })
 }
 
-function createImgElement(link) {
+function createImgElement(link, linkFull) {
     const img__element = document.createElement('img');
+    const aElement = document.createElement('a');
+    aElement.href = linkFull;
+
     img__element.classList.add('img__element');
     img__element.style.background = `url(${link})`;
-    return img__element;
+    aElement.appendChild(img__element);
+    return aElement;
 }
 
 
@@ -59,7 +62,6 @@ inputForm.addEventListener('keyup', (event) => {
             getPhotos();
         }
     }
-
 });
 
 deleteButton.addEventListener('click', (event) => {
