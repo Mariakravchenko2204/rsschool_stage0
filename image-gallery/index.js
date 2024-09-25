50f05ebf0a831f92e200ea266be65be7529c20f8
Mariia Kravchenko <mariia.kravchenko2204@gmail.com>
2024-09-24 21:05:25 -0400

    feat: add pagination

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,12 +1,22 @@
 const img__container = document.querySelector('.image__container');
 const inputForm = document.querySelector(".form-control");
 const searchButton = document.querySelector(".search__button");
+const deleteButton = document.querySelector(".remove__text");
+const nextButton = document.querySelector(".next");
+const current = document.querySelector(".current");
+const prevButton = document.querySelector(".prev");
+let page = 1;
+let query = 'Canada';
+let maxPages = 5;
 
 
-async function getPhotos(param) {
-    fetch(`https://api.unsplash.com/search/photos?query=${param}&per_page=18&orientation=landscape&client_id=dbXTMWsnem3eDUnb3j7Kva8_XcWwF7NCZYWt06_6bKk`)
+
+async function getPhotos() {
+    fetch(`https://api.unsplash.com/search/photos?query=${query}&page=${page}&per_page=18&orientation=landscape&client_id=dbXTMWsnem3eDUnb3j7Kva8_XcWwF7NCZYWt06_6bKk`)
         .then((res) => res.json())
         .then((data) => {
+            console.log(data)
+            totalPages = data.total_pages;
             img__container.innerHTML = '';
             data.results.map(e => {
                 const element = createImgElement(e.urls.small);
@@ -31,7 +41,8 @@ searchButton.addEventListener('click', () => {
     if(inputForm.value === ''){
         getPhotos('Canada')
     }else{
-        getPhotos(inputForm.value);
+        query = inputForm.value;
+        getPhotos();
     }
     
 })
@@ -41,8 +52,34 @@ inputForm.addEventListener('keyup', (event) => {
         if(inputForm.value === ''){
             getPhotos('Canada')
         }else{
-            getPhotos(inputForm.value);
+            query = inputForm.value;
+            getPhotos();
         }
     }
 
+});
+
+deleteButton.addEventListener('click', (event) => {
+    console.log(inputForm.value);
+    inputForm.value = '';
+})
+
+nextButton.addEventListener('click', () => {
+    page++;
+    current.innerHTML = page;
+    getPhotos();
+    prevButton.classList.remove('inactive');
+    if(page === maxPages){
+        nextButton.classList.add('inactive')
+    }
+})
+
+prevButton.addEventListener('click', () => {
+    page--;
+    current.innerHTML = page;
+    getPhotos();
+    nextButton.classList.remove('inactive')
+    if(page === 1){
+        prevButton.classList.add('inactive');
+    }
 })
